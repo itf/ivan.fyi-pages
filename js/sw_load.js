a68a8f9aa657f9ab0d0a4bcaeff568eedfc3d769
Ivan Tadeu Ferreira Antunes Filho <itadeufa@gmail.com>
2023-08-27 02:24:16 -0400

    Initial commit ivanfyi public website

---
+++ js/sw_load.js
@@ -0,0 +1,13 @@
+if ("serviceWorker" in navigator) {
+    navigator.serviceWorker
+        .register("/sw.min.js?v=3.8.1", { scope: "/" })
+        .then(() => {
+            //console.info("SW Loaded");
+        }, err => console.error("SW error: ", err));
+
+    navigator.serviceWorker
+        .ready
+        .then(() => {
+            //console.info("SW Ready");
+        });
+}
